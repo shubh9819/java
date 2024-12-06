bb1e1db7f682e6f42b3a1241c30a4dd70179fc46
shubham vishwakarma <167439889+shubh9819@users.noreply.github.com>
2024-12-06 16:16:16 +0530

    Add files via upload
    change

---
+++ access_control1/variabletype3.java
@@ -0,0 +1,13 @@
+package access_control1;
+
+import access_control.variabletype;
+
+public class variabletype3 {
+    public static void main(String[] args) {
+        variabletype abb=new variabletype();
+       // System.out.println(abb.a);
+        System.out.println(abb.b);
+       // System.out.println(abb.c);
+       // System.out.println(abb.d);
+    }
+}
